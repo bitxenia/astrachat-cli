b6ed241b3d15008f56130ab680b91dc3427d1f36
SBen-IV <sinneo@fi.uba.ar>
2025-04-28 19:28:26 -0300

    feat: create chat

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
-
 import { program } from "commander";
+import { createChatManager } from "@bitxenia/astrachat-eth";
 
 program.name("astrachat").description("Astrachat node").version("0.0.1");
+
+program
+  .command("create")
+  .option("-n --name <name>", "Name of the chat")
+  .action(async (opts) => {
+    const chatManager = await createChatManager();
+
+    chatManager.createChat(opts.name);
+  });
+
+program.parse(process.argv);
